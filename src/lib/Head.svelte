7eb2537a6a5e227fa8dda1d632bb7dd1ae2d02f7
bloshko <mis.bloko@gmail.com>
2024-01-30 18:47:06 +0100

    Update meta description

--- src/lib/Head.svelte
+++ src/lib/Head.svelte
@@ -5,5 +5,8 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="/nahaba_main_1x1.jpg" />
 	<meta name="robots" content="index, follow" />
-	<meta name="description" content="The official page of the ukrainian band NAHABA" />
+	<meta
+		name="description"
+		content="NAHABA, the Ukrainian musical duo featuring Andrii Naumenko and Olya Kakasyan. Listen to their new track GLASSS"
+	/>
 </svelte:head>
