560a176c3cee260422cfc8db84aa9ce3cc1d6f22
bloshko <mis.bloko@gmail.com>
2024-01-31 21:00:47 +0100

    Change favicon and update meta description

--- src/lib/Head.svelte
+++ src/lib/Head.svelte
@@ -1,12 +1,9 @@
 <svelte:head>
-	<title>NAHABA BAND</title>
+	<title>Український гурт NAHABA</title>
 	<meta property="og:title" content="NAHABA" />
-	<meta property="og:description" content="Ukrainian band NAHABA" />
+	<meta property="og:description" content="Український гурт NAHABA" />
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="/nahaba_main_1x1.jpg" />
 	<meta name="robots" content="index, follow" />
-	<meta
-		name="description"
-		content="NAHABA, the Ukrainian musical duo featuring Andrii Naumenko and Olya Kakasyan. Listen to their new track GLASSS"
-	/>
+	<meta name="description" content="Український гурт NAHABA" />
 </svelte:head>
